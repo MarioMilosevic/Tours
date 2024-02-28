2be9a30f47c51d6d9a2321d3c4086b5273253721
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-02-28 15:58:00 +0100

    Add heading component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,26 @@
-<template></template>
-
-<script setup></script>
+<template>
+    <Heading />
+  </template>
+  
+  <script>
+  import Heading from "./components/Heading.vue";
+  
+  export default {
+    name: "App",
+    data() {
+      return {
+        title: "My First Vue App",
+      };
+    },
+    methods: {
+     async handleClick() {
+        const response = await fetch("https://course-api.com/react-tours-project")
+        console.log(response)
+      },
+    },
+    components: {
+      Heading,
+    },
+  };
+  </script>
+  
---
+++ src/components/Heading.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="text-5xl py-6">
+    {{ title }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Vue Tours",
+    };
+  },
+};
+</script>
